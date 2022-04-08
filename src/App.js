64875ebcbbd8af18e91bc44b75c3c4439b326158
hankh0412 <hankh0412@naver.com>
2022-04-08 19:12:41 +0900

    chapter 2.3.3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+// react에서 useState와 useEffect Hook 가져오기
+import React, { useState, useEffect } from 'react'
+
+// AWS Amplify에서 API가져오기
+import { API } from 'aws-amplify'
+
+import './App.css'
 
 function App() {
+  // 빈 배열을 초깃값으로 coins 변수 생성
+  const [coins, updateCoins] = useState([])
+
+  // API 호출을 위한 fetchCoins 함수 정의
+  async function fetchCoins() {
+    const data = await API.get('cryptoapi', '/coins')
+    updateCoins(data.coins)
+  }
+
+  // 컴포넌트가 마운트될 때 fetchCoins 함수 호출
+  useEffect(() => {
+    fetchCoins()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {
+        coins.map((coin, index) => (
+          <div key={index}>
+            <h2>{coin.name} - {coin.symbol}</h2>
+            <h5>${coin.price_usd}</h5>
+          </div>
+        ))
+      }
     </div>
   );
 }
 
-export default App;
+export default App
